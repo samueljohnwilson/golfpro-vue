b7487419065d2ce47cd32b4ed6540350c0809933
Sam <s.wilson@echosec.net>
2019-12-28 12:09:33 -0800

    Add validation

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,37 +4,26 @@
       v-model="dialogState"
       width="500"
     >
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Privacy Policy
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="$emit('close-modal')"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <Login/>
     </v-dialog>
   </div>
 </template>
 <script>
+import Login from './Login';
+
 export default {
   name: 'Modal',
+
+  components: {
+    Login,
+  },
+
+  data: function() {
+    return {
+      email : "",
+      password : ""
+    }
+  },
 
   props: {
     dialog: Boolean,
@@ -50,6 +39,15 @@ export default {
           this.$emit('close-modal');
         }
       }
+    }
+  },
+
+  methods: {
+    login: function() {
+      let email = this.email 
+      let password = this.password
+      this.$store.dispatch('login', { email, password })
+        .then(() => this.$router.push('/'));
     }
   }
 };
